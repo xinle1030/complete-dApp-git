8f940df2dd369be8cce4a09aebbe2d45037bb627
LAMXINLE <xlam0003@student.monash.edu>
2023-02-05 16:33:35 +0800

    deploy all contracts

--- defi-staking-app-starter-main/defi-staking-app-starter/migrations/2_deploy_contracts.js
+++ defi-staking-app-starter-main/defi-staking-app-starter/migrations/2_deploy_contracts.js
@@ -1,5 +1,23 @@
 const Tether = artifacts.require("Tether");
+const RWD = artifacts.require("RWD");
+const DecentralBank = artifacts.require("DecentralBank");
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, network, accounts) {
+  // Deploy Mock Tether Token
   await deployer.deploy(Tether);
+  const tether = await Tether.deployed();
+
+  // Deploy RWD Token
+  await deployer.deploy(RWD);
+  const rwd = await RWD.deployed();
+
+  // Deploy DecentralBank
+  await deployer.deploy(DecentralBank, rwd.address, tether.address);
+  const decentralBank = await DecentralBank.deployed();
+
+  // Transfer all reward tokens to DecentralBank (1 million)
+  await rwd.transfer(decentralBank.address, "1000000000000000000000000");
+
+  // Transfer 100 Mock Tether tokens to investor
+  await tether.transfer(accounts[1], "100000000000000000000");
 };
