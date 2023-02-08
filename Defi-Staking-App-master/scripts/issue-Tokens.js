8b4941b5b5d4475eebe82beedab3442adf853d5a
LAMXINLE <xlam0003@student.monash.edu>
2023-02-08 17:31:32 +0800

    import artifacts into script

--- Defi-Staking-App-master/scripts/issue-Tokens.js
+++ Defi-Staking-App-master/scripts/issue-Tokens.js
@@ -1,9 +1,12 @@
+const { artifacts } = require("truffle");
 const DecentralBank = artifacts.require("DecentralBank");
 
 // scripts to issue tokens with a callback function as argument
-module.exports = async function issueRewards(callback) {
+async function issueRewards() {
   let decentralBank = await DecentralBank.deployed();
   await decentralBank.issueTokens();
   console.log("Tokens have been issued successfully !");
-  callback();
+  // callback();
 };
+
+export default issueRewards;
